e46948d53353b8e740c2ef44293e228dba8883f1
NET14_XuanBach <bachdxse182030@fpt.edu.vn>
2025-12-04 14:58:37 +0700

    update

--- mystic-tales-podcast-backend/subscription-service/SubscriptionService.BusinessLogic/DTOs/Report/PodcastSubscriptionDashboardResponseDTO.cs
+++ mystic-tales-podcast-backend/subscription-service/SubscriptionService.BusinessLogic/DTOs/Report/PodcastSubscriptionDashboardResponseDTO.cs
@@ -1,11 +1,10 @@
-﻿using SubscriptionService.BusinessLogic.DTOs.Report;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubscriptionService.BusinessLogic.DTOs.PodcastSubscriptionTransaction
+namespace SubscriptionService.BusinessLogic.DTOs.Report
 {
     public class PodcastSubscriptionDashboardResponseDTO
     {
