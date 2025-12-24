670d74f745acfed5e20f0607fc7aae5ab3acf8c3
Litpe2point0 <dxbach2004@gmail.com>
2025-12-24 14:30:10 +0700

    fix

--- mystic-tales-podcast-backend/booking-management-service/BookingManagementService.BusinessLogic/DTOs/Booking/ListItems/BookingMoneyFlowListItemResponseDTO.cs
+++ mystic-tales-podcast-backend/booking-management-service/BookingManagementService.BusinessLogic/DTOs/Booking/ListItems/BookingMoneyFlowListItemResponseDTO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingManagementService.BusinessLogic.DTOs.Transaction;
 
 namespace BookingManagementService.BusinessLogic.DTOs.Booking.ListItems
 {
